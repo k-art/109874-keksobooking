2ce6ce6af6d81e7503c76b0a4723c3ea6b156a30
Artem Kozlov <arte.horn@gmail.com>
2018-06-26 15:36:53 +0200

    дабавляет возможность передвигать главный маркер на карте

--- js/map.js
+++ js/map.js
@@ -112,10 +112,41 @@
     setMainPinAddress(evt);
   }
 
-  // Активация/деактивация страницы
-  mapPinMain.addEventListener('mouseup', function (evt) {
-    activatePage(evt);
-    renderMapPins(usersData);
+  // Перемещение пина по карте
+  mapPinMain.addEventListener('mousedown', function (evt) {
+
+    var startCoords = {
+      x: evt.clientX,
+      y: evt.clientY
+    };
+
+    function onMouseMove(moveEvt) {
+      moveEvt.preventDefault();
+
+      var shift = {
+        x: startCoords.x - moveEvt.clientX,
+        y: startCoords.y - moveEvt.clientY
+      };
+
+      startCoords = {
+        x: moveEvt.clientX,
+        y: moveEvt.clientY
+      };
+
+      mapPinMain.style.top = (mapPinMain.offsetTop - shift.y) + 'px';
+      mapPinMain.style.left = (mapPinMain.offsetLeft - shift.x) + 'px';
+    }
+
+    function onMouseUp(upEvt) {
+      upEvt.preventDefault();
+      document.removeEventListener('mousemove', onMouseMove);
+      document.removeEventListener('mouseup', onMouseUp);
+      activatePage(upEvt);
+      renderMapPins(usersData);
+    }
+
+    document.addEventListener('mousemove', onMouseMove);
+    document.addEventListener('mouseup', onMouseUp);
   });
 
   formButtonReset.addEventListener('click', function (evt) {
